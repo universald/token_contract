e98de013d76ff17697870aa769cf3505a018c74e
universald <43495298+universald@users.noreply.github.com>
2022-10-10 19:24:00 -0400

    moved the original test code back into test
    moved the original test code back into test

--- tests/token-contract.ts
+++ tests/token-contract.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { TokenContract } from "./target/types/token_contract";
+import { TokenContract } from "../target/types/token_contract";
 import {
   TOKEN_PROGRAM_ID,
   MINT_SIZE,
